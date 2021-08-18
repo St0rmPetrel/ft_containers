e3ac8cca94d1408c9ed7c60001f870c4ee18a08e
Shoots Erma <serma@pl-f1.msk.21-school.ru>
2021-08-18 21:12:07 +0300

    Add member types in vector

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include "vector.hpp"
+
+int main() {
+	std::vector<const int>	a;
+
+	//a.push_back(10);
+}
--- vector.hpp
+++ vector.hpp
@@ -11,11 +11,24 @@ namespace ft {
 			typedef typename allocator_type::const_reference const_reference;
 			typedef typename allocator_type::pointer pointer;
 			typedef typename allocator_type::const_pointer const_pointer;
-			//typedef random_acces_iterator ??
+
+			class iterator :
+				public std::iterator<std::random_access_iterator_tag,
+					value_type> {
+			};
+			class const_iterator :
+				public std::iterator<std::random_access_iterator_tag,
+					const value_type> {
+			};
+			typedef reverse_iterator<iterator> reverse_iterator;
+			typedef reverse_iterator<const_iterator> const_reverse_iterator;
+
+			typedef iterator_traits<iterator>::difference_type difference_type;
+			typedef size_t size_type;
 
 		private:
 			//bla bla
-	} /* class vector */
-} /* namespace ft */
+	}; /* class vector */
+}; /* namespace ft */
 
 #endif /* VECTOR_HPP */

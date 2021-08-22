2889a221003d532b6893bd8a77d3aef436b48bca
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2021-08-22 18:01:24 +0300

    Fix problem whith range and copy constructors
    Problem to differ range and fill constuctor in case of integral types
    Fix problem whith "enable_if" and "is_integral" templates
    Should create my own ft::is_intergral type by subject,
    now use std:: for testing

--- test.cpp
+++ test.cpp
@@ -1,9 +1,24 @@
 #include "vector.hpp"
 #include <iostream>
-#include <vector>
+// constructing vectors
+#include <iostream>
 
-int main() {
-	ft::vector<int> a(4);
+int main ()
+{
+  // constructors used in the same order as described above:
+  ft::vector<int> first;                                // empty vector of ints
+  ft::vector<int> second (4,100);                       // four ints with value 100
+  ft::vector<int> third (second.begin(),second.end());  // iterating through second
+  ft::vector<int> fourth (third);                       // a copy of third
 
-	ft::vector<int> b(a);
+  // the iterator constructor can also be used to construct from arrays:
+  int myints[] = {16,2,77,29};
+  ft::vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+
+  std::cout << "The contents of fifth are:";
+  for (ft::vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  return 0;
 }

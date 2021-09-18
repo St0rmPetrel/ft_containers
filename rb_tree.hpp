bc7290b1832dbb49ee83fbbc2e2687531496594a
Shoots Erma <serma@il-g5.msk.21-school.ru>
2021-09-18 20:06:58 +0300

    Add null_node and allocs in rb_tree

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -6,59 +6,74 @@
 namespace ft {
 	template <  class Key,
 			 	class T = Key,
-				class Compare = std::less<Key> >
+				class Compare = std::less<Key>,
+				class AllocatorKey = std::allocator<Key>,
+				class AllocatorT = std::allocator<T> >
 	class RBTree {
 		public:
-			typedef Key     key_type;
-			typedef T*      value_type;
-			typedef Compare key_compare;
+			typedef Key          key_type;
+			typedef T            value_type;
+			typedef Compare      key_compare;
+			typedef AllocatorKey key_allocator_type;
+			typedef AllocatorT   value_allocator_type;
 		private:
+			enum Color { Red, Black };
 			typedef struct Treenode {
-				key_type    key;
-				value_type  data;
+				key_type    *key;
+				value_type  *data;
+
+				Color       c;
 				Treenode    *left;
 				Treenode    *right;
 				Treenode    *p;
 
-				Treenode(key_type key = key_type(), value_type data = NULL)
-					: key(key), data(data), left(NULL), right(NULL), p(NULL) {}
+				Treenode()
+					: key(NULL),
+					data(NULL),
+					left(NULL),
+					right(NULL),
+					p(NULL),
+					c(Black) {}
 				~Treenode() {}
-
-				void build(const key_type& k, value_type d) {
-					this->key = k;
-					this->data = d;
-				}
-			} node_t;
+			} node_type;
 
 		public:
-			RBTree() : _root(NULL) {}
+			explicit RBTree(
+					const key_allocator_type& key_alloc = key_allocator_type(),
+					const value_allocator_type& value_alloc =
+						value_allocator_type())
+				: _key_alloc(key_alloc), _value_alloc(value_alloc) {
+				this->_null_node = new Treenode;
+				this->_root = this->_null_node;
+			}
 			~RBTree() {
 				this->_inorder_tree_delete(this->_root);
+				delete this->_null_node;
 			}
 
 			value_type& search(const key_type& k) {
 				return (_search(this->_root, k)->data);
 			}
 
-			void insert(const key_type& k, value_type d) {
-				node_t* new_node = new Treenode;
-				new_node->build(k, d);
-				_root = new_node;
-			}
 		private:
-			void _inorder_tree_delete(node_t *x) {
-				if (x != NULL) {
+			void _inorder_tree_delete(node_type *x) {
+				if (!this->_is_null_node(x)) {
 					_inorder_tree_delete(x->left);
-					node_t *next = x->right;
+					node_type *next = x->right;
+					//delte key
+					_key_alloc.destroy(x->key);
+					_key_alloc.deallocate(x->key, 1);
+					//delte data
 					if (x->data != NULL) {
-						delete x->data;
+						_value_alloc.destroy(x->data);
+						_value_alloc.deallocate(x->data, 1);
 					}
 					delete x;
 					_inorder_tree_delete(next);
 				}
 			}
-			node_t* _search(node_t* x, const key_type& k) {
-				if (x == NULL || k == x->key)
+			node_type* _search(node_type* x, const key_type& k) {
+				if (this->_is_null_node(x) || k == x->key)
 					return x;
 				if (key_compare(k, x->key)) {
 					return _search(x->left, k);
@@ -66,14 +81,14 @@ namespace ft {
 					return _search(x->right, k);
 				}
 			}
-			void _left_rotate(node_t* x) {
-				node_t* y = x->right;
+			void _left_rotate(node_type* x) {
+				node_type* y = x->right;
 				x->right = y->left;
-				if (y->left != NULL) {
+				if (!this->_is_null_node(y->left)) {
 					y->left->parent = x;
 				}
 				y->parent = x->parent;
-				if (x->parent == NULL) {
+				if (!this->_is_null_node(x->parent)) {
 					this->_root = y;
 				} else if (x == x->parent->left) {
 					x->parent->left = y;
@@ -83,14 +98,14 @@ namespace ft {
 				y->left = x;
 				x->parent = y;
 			}
-			void _right_rotate(node_t* y) {
-				node_t* x = y->left;
+			void _right_rotate(node_type* y) {
+				node_type* x = y->left;
 				y->left = x->right;
-				if (x->right != NULL) {
+				if (!this->_is_null_node(x->left)) {
 					x->right->parent = y;
 				}
 				x->parent = y->parent;
-				if (y->parent == NULL) {
+				if (!this->_is_null_node(y->parent)) {
 					this->_root = x;
 				} else if (y == y->parent->left) {
 					y->parent->left = x;
@@ -100,9 +115,19 @@ namespace ft {
 				x->right = y;
 				y->parent = x;
 			}
+			bool _is_null_node(node_type* x) {
+				if (x->key == NULL) {
+					return true;
+				}
+				return false;
+			}
 
 		private:
-			node_t *_root;
+			node_type*           _root;
+			node_type*           _null_node;
+
+			key_allocator_type   _key_alloc;
+			value_allocator_type _value_alloc;
 	};
 }
 

5fc0c189190b2e92e76427d4f553bfd429321f44
Rzaev Telman <rzaev.telman.tarik@gmail.com>
2021-12-12 11:01:51 +0300

    Fix bug in Insert, and Delete start working

--- rb_tree.hpp
+++ rb_tree.hpp
@@ -4,9 +4,7 @@
 #include <functional>
 #include <memory>
 
-//DEBUG
 #include <iostream>
-//DEBUG
 
 namespace ft {
 	template <  class Key,
@@ -83,54 +81,112 @@ namespace ft {
 
 			void insert_node(const key_type& k, value_type* data = NULL) {
 				node_type* new_node = _create_node(k, data);
-				_bst_insert_node(new_node);
-				_insert_fixup(new_node);
-			}
 
-			void delete_node(const key_type& k) {
-				// Stolen part of code :^(
-			}
+				node_type* y = NULL;
+				node_type* x = this->_root;
 
-			//DEBUG
-			void debug() {
-				this->_debug(this->_root);
-			}
-			//DEBUG
-
-		private:
-			//DEBUG
-			void _debug(node_type *x) {
-				if (!this->_is_null_node(x)) {
-					_debug(x->left);
-					std::cout << *(x->key) << std::endl;
-					_debug(x->right);
-				}
-			}
-			//DEBUG
-			void _bst_insert_node(node_type* z) {
-				node_type* y = _null_node;
-				node_type* x = _root;
 				while (!_is_null_node(x)) {
 					y = x;
-					if (_key_cmp(*(z->key), *(x->key))) {
+					if (_key_cmp(*(new_node->key), *(x->key))) {
+					//if (*(new_node->key) < *(x->key)) {
 						x = x->left;
 					} else {
 						x = x->right;
 					}
 				}
-				x->p = y;
-				if (_is_null_node(y)) {
-					_root = z;
+
+				new_node->p = y;
+				if (y == NULL) {
+					this->_root = new_node;
+				//} else if (*(new_node->key) < *(y->key)) {
+				} else if (_key_cmp(*(new_node->key), *(y->key))) {
+					y->left = new_node;
 				} else {
-					if (_key_cmp(*(z->key), *(y->key))) {
-						y->left = z;
+					y->right = new_node;
+				}
+
+				if (new_node->p == NULL) {
+					new_node->c = Black;
+					return;
+				}
+
+				if (new_node->p->p == NULL) {
+					return;
+				}
+
+				_insert_fixup(new_node);
+			}
+
+			void delete_node(const key_type& k) {
+				node_type* z = _search(this->_root, k);
+				if (_is_null_node(z)) {
+					return;
+				}
+				_delete_node(z);
+			}
+
+			void debug() {
+				this->_debug(this->_root);
+			}
+
+		private:
+			void _debug(node_type *x) {
+				if (!this->_is_null_node(x)) {
+					_debug(x->left);
+					std::cout << *(x->key) << " ";
+					_debug(x->right);
+				}
+			}
+			void _delete_node(node_type* z) {
+				node_type *x, *y;
+				Color y_original_color;
+
+				if (_is_null_node(z) || z == NULL) {
+					return;
+				}
+
+				y = z;
+				y_original_color = y->c;
+				if (_is_null_node(z->left)) {
+					x = z->right;
+					_transplant(z, z->right);
+				} else if (_is_null_node(z->right)) {
+					x = z->left;
+					_transplant(z, z->left);
+				} else {
+					y = _minimum(z->right);
+					y_original_color = y->c;
+					x = y->right;
+					if (y->p == z) {
+						x->p = y;
 					} else {
-						y->right = z;
+						_transplant(y, y->right);
+						y->right = z->right;
+						y->right->p = y;
 					}
+					_transplant(z, y);
+					y->left = z->left;
+					y->left->p = y;
+					y->c = z->c;
+				}
+
+				// Delete node z
+				//delte key
+				_key_alloc.destroy(z->key);
+				_key_alloc.deallocate(z->key, 1);
+				//delte data
+				if (z->data != NULL) {
+					_value_alloc.destroy(z->data);
+					_value_alloc.deallocate(z->data, 1);
+				}
+				delete z;
+
+				if (y_original_color == Black) {
+					_delete_fixup(x);
 				}
 			}
 			void _inorder_tree_delete(node_type *x) {
-				if (!this->_is_null_node(x)) {
+				if (!_is_null_node(x)) {
 					_inorder_tree_delete(x->left);
 					node_type *next = x->right;
 					//delte key
@@ -146,15 +202,17 @@ namespace ft {
 				}
 			}
 			void _insert_fixup(node_type *z) {
-				node_type* y = _null_node;
+				node_type* y;
+
 				while (z->p->c == Red) {
 					// Z is son of left parent (has righ uncle)
 					if (z->p == z->p->p->left) {
 						y = z->p->p->right;
 						// Red uncle (just recolor)
 						if (y->c == Red) {
+							y->c = Black;
 							z->p->c = Black;
-							y->c = Red;
+							z->p->p->c = Red;
 							z = z->p->p;
 						// Black uncle (with rotation)
 						} else {
@@ -171,8 +229,9 @@ namespace ft {
 						y = z->p->p->left;
 						// Red uncle (just recolor)
 						if (y->c == Red) {
+							y->c = Black;
 							z->p->c = Black;
-							y->c = Red;
+							z->p->p->c = Red;
 							z = z->p->p;
 						// Black uncle (with rotation)
 						} else {
@@ -185,19 +244,21 @@ namespace ft {
 							_left_rotate(z->p->p);
 						}
 					}
+					if (z == this->_root) {
+						break;
+					}
 				}
 				_root->c = Black;
 			}
-			// Stolen part of code
 			void _delete_fixup(node_type* x) {
 				node_type* s;
 				while (x != this->_root && x->c == Black) {
 					if (x == x->p->left) {
-						s = x->parent->right;
+						s = x->p->right;
 						if (s->c == Red) {
 							s->c = Black;
 							x->p->c = Red;
-							_left_rotate(x->parent);
+							_left_rotate(x->p);
 							s = x->p->right;
 						}
 
@@ -253,10 +314,23 @@ namespace ft {
 				ret->data = data;
 				ret->key = _key_alloc.allocate(1);
 				_key_alloc.construct(ret->key, k);
-				ret->p = _null_node;
+				ret->p = NULL;
 				ret->left = _null_node;
 				ret->right = _null_node;
+				ret->c = Red;
 				return ret;
+			}
+			node_type* _minimum(node_type* x) {
+				while (!_is_null_node(x->left)) {
+					x = x->left;
+				}
+				return x;
+			}
+			node_type* _maximum(node_type* x) {
+				while (!_is_null_node(x->right)) {
+					x = x->right;
+				}
+				return x;
 			}
 			node_type* _search(node_type* x, const key_type& k) {
 				if (this->_is_null_node(x) || k == *(x->key))
@@ -275,7 +349,7 @@ namespace ft {
 					y->left->p = x;
 				}
 				y->p = x->p;
-				if (!_is_null_node(x->p)) {
+				if (x->p == NULL) {
 					this->_root = y;
 				} else if (x == x->p->left) {
 					x->p->left = y;
@@ -285,22 +359,22 @@ namespace ft {
 				y->left = x;
 				x->p = y;
 			}
-			void _right_rotate(node_type* y) {
-				node_type* x = y->left;
-				y->left = x->right;
-				if (!_is_null_node(x->left)) {
-					x->right->p = y;
+			void _right_rotate(node_type* x) {
+				node_type* y = x->left;
+				x->left = y->right;
+				if (!_is_null_node(y->right)) {
+					y->right->p = x;
 				}
-				x->p = y->p;
-				if (!_is_null_node(y->p)) {
-					this->_root = x;
-				} else if (y == y->p->left) {
-					y->p->left = x;
+				y->p = x->p;
+				if (x->p == NULL) {
+					this->_root = y;
+				} else if (x == x->p->right) {
+					x->p->right = y;
 				} else {
-					y->p->right = x;
+					x->p->left = y;
 				}
-				x->right = y;
-				y->p = x;
+				y->right = x;
+				x->p = y;
 			}
 			void _transplant(node_type* u, node_type* v) {
 				if (_is_null_node(u->p)) {
@@ -314,6 +388,9 @@ namespace ft {
 			}
 
 			bool _is_null_node(node_type* x) {
+				if (x == NULL) {
+					return true;
+				}
 				if (x->key == NULL) {
 					return true;
 				}

6a60981e4407ae74be7876b535389d0d089488d3
Rzaev Telman <rzaev.telman.tarik@gmail.com>
2021-12-18 21:56:03 +0300

    Add pair class

---
+++ pair.hpp
@@ -0,0 +1,58 @@
+#ifndef PAIR_HPP
+#define PAIR_HPP
+
+namespace ft {
+	template <class T1, class T2>
+	struct pair {
+		typedef T1 first_type;
+		typedef T2 second_type;
+	
+		first_type  first;
+		second_type second;
+	
+		// Constructor
+		// default
+		pair() { }
+		// copy
+		template<class U, class V>
+		pair (const pair<U,V>& pr) : first(pr.first), second(pr.second) { }
+		// initialization
+		pair (const first_type& a, const second_type& b) : first(a), second(b) { }
+		// Destructor
+		~pair() { }
+	
+		pair& operator= (const pair& pr) {
+			this->first = pr.first;
+			this->second = pr.second;
+			return (*this);
+		}
+	};
+	
+	// Relational operators
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return lhs.first==rhs.first && lhs.second==rhs.second;
+	}
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return !(lhs==rhs);
+	}
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second);
+	}
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return !(rhs<lhs);
+	}
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return rhs<lhs;
+	}
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs) {
+		return !(lhs<rhs);
+	}
+}; /* namespace ft */
+
+#endif /* PAIR_HPP */
